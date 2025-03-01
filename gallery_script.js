efde3156f68d34a7768d6d02224a18e5e40c9e19
Theonion400 <1425461@edu.burnabyschools.ca>
2025-03-01 11:34:21 -0800

    added commas

--- gallery_script.js
+++ gallery_script.js
@@ -1,5 +1,5 @@
 const imageGallery =[
-    "https://images.pexels.com/photos/2085831/pexels-photo-2085831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2,"
-    "https://images.pexels.com/photos/981588/pexels-photo-981588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-    "https://images.pexels.com/photos/670003/pexels-photo-670003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    "https://images.pexels.com/photos/2085831/pexels-photo-2085831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/981588/pexels-photo-981588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/670003/pexels-photo-670003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
 ]
