41ab56b86461c99471de92b61cea6ea495cb8f65
Theonion400 <1425461@edu.burnabyschools.ca>
2025-03-08 11:05:05 -0800

    added button script

--- gallery_script.js
+++ gallery_script.js
@@ -2,7 +2,7 @@ const imageGallery =[
     "https://images.pexels.com/photos/2085831/pexels-photo-2085831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "https://images.pexels.com/photos/981588/pexels-photo-981588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "https://images.pexels.com/photos/670003/pexels-photo-670003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    "https://images.unsplash.com/photo-1625314868143-20e93ce3ff33?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    "https://images.unsplash.com/photo-1625314868143-20e93ce3ff33?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ]
 
 const imageElement = document.getElementById("gallery-image");
@@ -16,7 +16,11 @@ function updateImage() {
 }
 
 nextButton.addEventListener("click", function(){
-    
-})
+    currentImgIndex = (currentImgIndex + 1) % imageGallery.length updateImage()
+});
+
+prevButton.addEventListener("click", function(){
+    currentImgIndex = (currentImgIndex - 1 + imageGallery.length) % imageGallery.length updateImage()
+});
 
 updateImage();
