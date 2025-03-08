deccc57c8a18cc6d4dca99775c5f64eba09bbbb4
Theonion400 <1425461@edu.burnabyschools.ca>
2025-03-08 10:38:09 -0800

    button fixed and added

--- gallery_script.js
+++ gallery_script.js
@@ -2,4 +2,11 @@ const imageGallery =[
     "https://images.pexels.com/photos/2085831/pexels-photo-2085831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "https://images.pexels.com/photos/981588/pexels-photo-981588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "https://images.pexels.com/photos/670003/pexels-photo-670003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.unsplash.com/photo-1625314868143-20e93ce3ff33?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 ]
+
+const imageElement = document.getElementById("gallery-image");
+const prevButton = document.getElementById("prev-btn");
+const nextButton = document.getElementById("next-btn");
+
+let currenImgIndex = 0;
