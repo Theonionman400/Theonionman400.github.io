95503b0773f445840fcafa2386be99fbc6e2d918
Theonion400 <1425461@edu.burnabyschools.ca>
2025-03-08 11:47:15 -0800

    new background imageand new parts

--- gallery_script.js
+++ gallery_script.js
@@ -3,6 +3,7 @@ const imageGallery =[
     "https://images.pexels.com/photos/981588/pexels-photo-981588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "https://images.pexels.com/photos/670003/pexels-photo-670003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "https://images.unsplash.com/photo-1625314868143-20e93ce3ff33?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1604138659235-30cd5e27b49f?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ]
 
 const imageElement = document.getElementById("gallery-image");
